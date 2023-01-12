ddc9b4fd285b32a7382f5d121d37db606b586231
Wojciech <wojtekb30.player@gmail.com>
2023-01-12 22:46:08 +0100

    Dodaj pliki projektów.

---
+++ Clipboard saver/Form1.Designer.vb
@@ -0,0 +1,137 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Formularz przesłania metodę dispose, aby wyczyścić listę składników.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wymagane przez Projektanta formularzy systemu Windows
+    Private components As System.ComponentModel.IContainer
+
+    'UWAGA: następująca procedura jest wymagana przez Projektanta formularzy systemu Windows
+    'Możesz to modyfikować, używając Projektanta formularzy systemu Windows. 
+    'Nie należy modyfikować za pomocą edytora kodu.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.SaveBtn = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.CheckBoxTime = New System.Windows.Forms.CheckBox()
+        Me.CheckBoxDate = New System.Windows.Forms.CheckBox()
+        Me.ButtonRestore = New System.Windows.Forms.Button()
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'SaveBtn
+        '
+        Me.SaveBtn.AutoSize = True
+        Me.SaveBtn.Location = New System.Drawing.Point(3, 3)
+        Me.SaveBtn.Name = "SaveBtn"
+        Me.SaveBtn.Size = New System.Drawing.Size(267, 58)
+        Me.SaveBtn.TabIndex = 0
+        Me.SaveBtn.Text = "Save to text file"
+        Me.SaveBtn.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 1000
+        '
+        'ListBox1
+        '
+        Me.ListBox1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.HorizontalScrollbar = True
+        Me.ListBox1.ItemHeight = 31
+        Me.ListBox1.Location = New System.Drawing.Point(3, 67)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.ScrollAlwaysVisible = True
+        Me.ListBox1.Size = New System.Drawing.Size(1425, 655)
+        Me.ListBox1.TabIndex = 1
+        '
+        'CheckBoxTime
+        '
+        Me.CheckBoxTime.AutoSize = True
+        Me.CheckBoxTime.Location = New System.Drawing.Point(294, 15)
+        Me.CheckBoxTime.Name = "CheckBoxTime"
+        Me.CheckBoxTime.Size = New System.Drawing.Size(165, 36)
+        Me.CheckBoxTime.TabIndex = 2
+        Me.CheckBoxTime.Text = "Add time"
+        Me.CheckBoxTime.UseVisualStyleBackColor = True
+        '
+        'CheckBoxDate
+        '
+        Me.CheckBoxDate.AutoSize = True
+        Me.CheckBoxDate.Location = New System.Drawing.Point(484, 15)
+        Me.CheckBoxDate.Name = "CheckBoxDate"
+        Me.CheckBoxDate.Size = New System.Drawing.Size(167, 36)
+        Me.CheckBoxDate.TabIndex = 3
+        Me.CheckBoxDate.Text = "Add date"
+        Me.CheckBoxDate.UseVisualStyleBackColor = True
+        '
+        'ButtonRestore
+        '
+        Me.ButtonRestore.Location = New System.Drawing.Point(679, 3)
+        Me.ButtonRestore.Name = "ButtonRestore"
+        Me.ButtonRestore.Size = New System.Drawing.Size(340, 59)
+        Me.ButtonRestore.TabIndex = 4
+        Me.ButtonRestore.Text = "Copy back to clipboard"
+        Me.ButtonRestore.UseVisualStyleBackColor = True
+        '
+        'Panel1
+        '
+        Me.Panel1.AutoSize = True
+        Me.Panel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.Panel1.Controls.Add(Me.SaveBtn)
+        Me.Panel1.Controls.Add(Me.ButtonRestore)
+        Me.Panel1.Controls.Add(Me.CheckBoxTime)
+        Me.Panel1.Controls.Add(Me.CheckBoxDate)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(1425, 65)
+        Me.Panel1.TabIndex = 5
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(16.0!, 31.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1425, 721)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Margin = New System.Windows.Forms.Padding(8, 7, 8, 7)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.WindowsDefaultBounds
+        Me.Text = "Clipboard Saver"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents SaveBtn As Button
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents CheckBoxTime As CheckBox
+    Friend WithEvents CheckBoxDate As CheckBox
+    Friend WithEvents ButtonRestore As Button
+    Friend WithEvents SaveFileDialog1 As SaveFileDialog
+    Friend WithEvents Panel1 As Panel
+End Class
---
+++ Clipboard saver/Form1.vb
@@ -0,0 +1,91 @@
+﻿Public Class Form1
+
+
+
+    Private Sub SaveBtn_Click(sender As Object, e As EventArgs) Handles SaveBtn.Click
+        SaveFileDialog1.Filter = "TXT|*.txt"
+        SaveFileDialog1.Title = "Save .txt"
+        SaveFileDialog1.ShowDialog()
+        Dim sciezka As String = SaveFileDialog1.FileName()
+
+        If sciezka <> "" Then
+            My.Computer.FileSystem.WriteAllText(sciezka, "", False)
+            For Each item As String In ListBox1.Items
+                My.Computer.FileSystem.WriteAllText(sciezka, item + Environment.NewLine + Environment.NewLine, True)
+            Next
+            MsgBox("File saved as " + sciezka)
+        Else
+            MsgBox("Nothing was saved. Try again.")
+        End If
+
+    End Sub
+
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+
+        Dim wycinek As String = My.Computer.Clipboard.GetText()
+        Dim OstatniaTabela As String = ListBox1.Items(0).ToString()
+        Dim IloscDoUsun As Int16 = 0
+        If CheckBoxTime.Checked And OstatniaTabela.Length > 10 Then
+            IloscDoUsun = 10
+        End If
+        If CheckBoxDate.Checked And OstatniaTabela.Length > 12 Then
+            IloscDoUsun = 12
+        End If
+        If CheckBoxDate.Checked And CheckBoxTime.Checked And OstatniaTabela.Length > 22 Then
+            IloscDoUsun = 22
+        End If
+        OstatniaTabela = OstatniaTabela.Remove(0, IloscDoUsun)
+
+        Dim wynik As Int16 = OstatniaTabela = wycinek
+
+
+        If wynik = 0 Then
+            Dim czas As String = ""
+            Dim rok As String = ""
+            If CheckBoxTime.Checked Then
+                czas = DateTime.Now.ToString("  HH:mm:ss")
+
+            End If
+
+            If CheckBoxDate.Checked Then
+                rok = DateTime.Now.ToString("  yyyy/MM/dd")
+            End If
+
+            Dim okres As String = rok + czas + ": "
+            Dim Informacja As String = okres + wycinek
+            Informacja = Informacja.Remove(0, 2)
+
+
+            Dim dane As New List(Of String)
+            For Each item As String In ListBox1.Items
+                dane.Add(item)
+            Next
+            dane.Reverse()
+            dane.Add(Informacja)
+            dane.Reverse()
+
+
+            ListBox1.DataSource = dane
+            ListBox1.Update()
+
+
+
+        End If
+
+
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ListBox1.Items.Add("---Program started---")
+    End Sub
+
+    Private Sub ButtonRestore_Click(sender As Object, e As EventArgs) Handles ButtonRestore.Click
+        Clipboard.SetText(ListBox1.SelectedItem.ToString())
+        MsgBox("''" + ListBox1.SelectedItem.ToString() + "'' copied to clipboard.")
+    End Sub
+End Class
